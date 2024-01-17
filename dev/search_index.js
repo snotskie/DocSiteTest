d330047a272e2e7ededc2f9be0107c12a4e4fe30
Documenter.jl <documenter@juliadocs.github.io>
2024-01-17 17:02:37 +0000

    build based on 0997eeb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Landing-Page","page":"Landing Page","title":"Landing Page","text":"","category":"section"},{"location":"","page":"Landing Page","title":"Landing Page","text":"landing page","category":"page"},{"location":"example/#Example","page":"Example","title":"Example","text":"","category":"section"},{"location":"example/","page":"Example","title":"Example","text":"example","category":"page"}]
+}
